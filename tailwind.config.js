757bca90c5b5b148587db1eb9e5a94f8c5618992
Tim Knops <timknops22@gmail.com>
2023-08-25 12:30:19 +0200

    Removing purge line config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/*.html", "./src/js/*.js"],
-  purge: ["./src/*.html", "./src/js/*.js"],
   theme: {
     extend: {},
   },
